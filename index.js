3be46f58cb511f9d9354b2dc215e45602576bbe6
deanmastria <dean.mastria@gmail.com>
2024-07-05 17:18:48 -0400

    notes added

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const numbers1 = [1, 2, 3, 4, 5, 6];
-const numbers2 = [11, 12, 13, 14, 15, 16];
-const numbers = [...numbers1, ...numbers2];
+const numbers1 = [1, 2, 3, 4, 5, 6];                                                                                // Create two arrays of numbers called numbers1 and numbers2
+const numbers2 = [11, 12, 13, 14, 15, 16];                                                                          
 
-const square = num => num * num;
+const numbers = [...numbers1, ...numbers2];                                                                         // Use the spread operator to create an array called numbers that combines the elements of numbers1 and numbers2
 
-const squares = numbers.map(square);
+const square = num => num * num;                                                                                    // Define an arrow function called square that accepts a number as an argument and returns the square of the number
 
-const isEven = number => number % 2 === 0;
 
-const evenSquares = numbers.filter(isEven);
+const squares = numbers.map(square);                                                                                // Use the map method to create a new array called squares that contains the squares of the numbers in the numbers array
 
-const [firstEvenSquare, secondEvenSquares] = evenSquares;
 
-console.log(`Original numbers: ${numbers}`);
+const isEven = number => number % 2 === 0;                                                                          // Define an arrow function called isEven that accepts a number as an argument and returns true if the number is even, otherwise false
+
+
+const evenSquares = numbers.filter(isEven);                                                                         // Use the filter method to create a new array called evenSquares that contains only the even squares from the squares array
+
+
+const [firstEvenSquare, secondEvenSquares] = evenSquares;                                                           // Use destructuring assignment to extract the first and second elements of the evenSquares array into constants firstEvenSquare and secondEvenSquare
+
+
+console.log(`Original numbers: ${numbers}`);                                                                        // Use a template literal to log the original numbers array, the squares array, the evenSquares array, and the extracted even squares                                                                   
 console.log(`Squares: ${squares}`);
 console.log(`Even squares: ${evenSquares}`);
 console.log(`First even square: ${firstEvenSquare}, Second even squares: ${secondEvenSquares}`);
